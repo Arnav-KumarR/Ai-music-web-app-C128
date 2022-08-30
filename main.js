4064efd7a8c4a7e89c2ab91027ee54b69545ca1c
Arnav-KumarR <89586230+Arnav-KumarR@users.noreply.github.com>
2022-08-30 22:51:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ rightWristX = 0;
 rightWristY = 0;
 
 function preload() {
-    song = loadSound("music.mp3");
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
 }
 
 function setup() {
